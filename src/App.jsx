946a11061c8e45c55c10ea00d0867f7bd1234331
cordovabarbara <aliendocordova@gmail.com>
2023-04-29 11:44:52 -0500

    creacion de nuevo portafolio

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+  <div className='bg-site bg-no-repeat bg-cover overflow-hidden'>
+    <Header/>
+    <Banner />
+    <About />
+    <Projects/>
+  </div>
+  )
+}
+
+export default App
