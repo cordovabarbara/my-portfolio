8f667f7ad345134c3799284ef514aa5c751159a6
cordovabarbara <aliendocordova@gmail.com>
2023-04-29 23:19:51 -0500

    se agregaron estilos a la seccion about y a la seccion banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import {TypeAnimation} from 'react-type-animation'
+import "./styles/banner.css";
 
 const Banner = () => {
   return (
     <div>
-      <h1> Barbara <span>Cordova</span></h1>
+      <h1 className='title__banner'> Barbara <span>Cordova</span></h1>
       <TypeAnimation
         sequence={[
           'Soy, desarrollador Front-end.',
           500,
         ]}
+        wrapper="h3"
         repeat={Infinity}
         />
     </div>
