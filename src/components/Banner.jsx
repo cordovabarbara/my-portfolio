f4343560d221110dc8e07d9c9bfd924e6a85a7f8
cordovabarbara <aliendocordova@gmail.com>
2023-05-02 12:49:23 -0500

    se ajustaron los estilos y padding

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -7,6 +7,12 @@ const Banner = () => {
   return (
     <div className='banner'>
       <h1 className='title__banner'> Barbara <span className='title__span'>Cordova</span></h1><div/>
+          <div className='icons__banner'>
+          <i class='bx bxl-github'></i>
+          <i class='bx bxl-linkedin-square'></i>
+          <i class='bx bxl-gmail' ></i>
+          </div>
+
           <div className='title__animation'>
           <TypeAnimation
             sequence={[
