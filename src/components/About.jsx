49f35fb18b9b523c062e744a9b8c12d1b4dfba31
cordovabarbara <aliendocordova@gmail.com>
2023-08-04 09:57:57 -0500

    fix animation

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,50 +1,23 @@
-import React, { useEffect, useState } from 'react'
+
 import perfil1 from '/public/perfil1.png'
 import "./styles/about.css";
 
 const About = () => {
-  const [visible, setVisible] = useState(false);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollY = window.scrollY;
-      const element = document.getElementById("sobre-mi");
-
-      if (element) {
-        const elementTop = element.offsetTop;
-        const windowHeight = window.innerHeight;
-
-        if (scrollY > elementTop - windowHeight / 2) {
-          setVisible(true);
-        }
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
   return (
     <> 
     <h1 id='sobre-mi' className='title__about'>Sobre Mi</h1>
-      <div className='about'>
-        <img className={`about__img ${visible ? 'visible' : ''}`} src={perfil1} alt=''></img>
-        <div className={`about__txt ${visible ? 'visible' : ''}`}>
+      <div className='about' data-aos="fade-right">
+        <img className="about__img" src={perfil1} alt=''></img>
+          <div className='about__txt'>
       Soy Barbara Córdova, oriunda de Caracas, Venezuela, actualmente resido en Bogotá, Colombia.
     Tengo conocimientos en diversos lenguajes de programación como JavaScript, HTML, CSS, React; y actualmente me estoy capacitando en el desarrollo de aplicaciones moviles. 
     Disfruto crear aplicaciones web que mejoren la experiencia de los usuarios.
     En pocas palabras, me considero alguien curiosa y fascinada por el mundo de la tecnología, el arte y el aprendizaje constante. Mi deseo es unirme a una empresa que me brinde la oportunidad de seguir aprendiendo y al mismo tiempo seguir desarrollándome como profesional.
     </div>
-  </div>
+    </div>
     
-    
-    </>
-
-  
-  )
+  </>
+)
 }
 
 export default About
