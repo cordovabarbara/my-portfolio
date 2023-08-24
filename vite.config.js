33391dc4a78a015e01b01b59e64048ae84a6dc24
cordovabarbara <aliendocordova@gmail.com>
2023-08-23 21:43:19 -0500

    fixed images

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server:{
-    open: true,
-    host: true
+    host:true
   }
 })
