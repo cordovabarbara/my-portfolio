6e348e99df7494961f748c650000edf98649afd6
cordovabarbara <aliendocordova@gmail.com>
2023-08-25 18:11:40 -0500

    created modal for projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,17 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./styles/projects.css";
 import images from "../utils/images"
 
 
-
 const Projects = () => {
+  const [showModal, setShowModal] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const openModal = (images) => {
+    setSelectedImage(images);
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setSelectedImage(null);
+    setShowModal(false);
+  };
+  
   return (
     <div className='project__box'>
     <span className='project__title' id='projects'>Proyectos</span>
     <div  className='project__principal'>
-      <img className='project__img'src={images.image1} alt="" />
-      <img className='project__img' src={images.imagen2} alt="" />
+      <img className='project__img'src={images.image1} alt="" onClick={() => openModal(images.image1)}/>
+      <img className='project__img' src={images.imagen2} alt="" onClick={() => openModal(images.imagen2)}/>
     </div>
+    {showModal && (
+         <div className={`modal ${showModal ? 'active' : ''}`} onClick={closeModal}>
+          <div className="modal-content">
+            <span className="close" onClick={closeModal}>&times;</span>
+            <img className="modal-image" src={selectedImage} alt="" />
+          </div>
+        </div>
+    )}
     </div>
  
  
