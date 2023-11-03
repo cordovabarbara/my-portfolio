5a72d88c1e36bd7f3773cd42fba279d44c14d945
cordovabarbara <aliendocordova@gmail.com>
2023-11-03 14:53:15 -0500

    fixed projects

--- src/utils/projectsData.js
+++ src/utils/projectsData.js
@@ -12,11 +12,11 @@ const projectsData = [
     },
     {
       title: 'Pokedex',
-      description: 'En esta aplicacion podemos consultar el clima y cambiar de farenheit a centigrados y viceversa, adicionalmente podemos consultar el clima de otras ciudades, utilizando la barra de busqueda',
-      tecnologies: 'Javascript, React, CSS',
+      description: 'Se consumio la API de pokemon para crear una aplicacion de consulta, donde el usuario podra ver todos los pokemones y sus caractericticas, ademas de  filtralos por tipo',
+      tecnologies: 'Javascript, React, Router Dom, Redux, CSS',
       image: imagens.poke,
-      demo: 'https://barbara-cordova-weatherapp.netlify.app/',
-      git: 'https://github.com/cordovabarbara/weather-app.git'
+      demo: 'https://poke-api-barbara-cordova.netlify.app/',
+      git: 'https://github.com/cordovabarbara/poke-api.git'
      
     },
 
