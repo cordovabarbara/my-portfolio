6045d6fd2455b79ec537222cf3218d2c8b1c0fd7
cordovabarbara <aliendocordova@gmail.com>
2024-02-16 19:04:17 -0500

    fix about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,11 +9,11 @@ const About = () => {
       <div className="about" data-aos="fade-right">
         <div className="about__txt">
           Soy Barbara Córdova, oriunda de Caracas, Venezuela, actualmente resido
-          en Bogotá, Colombia. Tengo conocimientos en diversos lenguajes de
-          programación como JavaScript, HTML, CSS, React; y actualmente me estoy
-          capacitando en el desarrollo de aplicaciones moviles. Disfruto crear
-          aplicaciones web que mejoren la experiencia de los usuarios. En pocas
-          palabras, me considero alguien curiosa y fascinada por el mundo de la
+          en Bogotá, Colombia. Tengo conocimientos en diversas tecnologias de
+          programación como JavaScript, HTML, CSS, React. Actualmente, estoy ampliando mis conocimientos 
+          en el área de backend, centrándome en el aprendizaje de la creación de bases de datos.
+          Disfruto crear aplicaciones web que mejoren la experiencia de los usuarios. 
+          En pocas palabras, me considero alguien curiosa y fascinada por el mundo de la
           tecnología, el arte y el aprendizaje constante. Mi deseo es unirme a
           una empresa que me brinde la oportunidad de seguir aprendiendo y al
           mismo tiempo seguir desarrollándome como profesional.
